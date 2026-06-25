3e99ba04316968b56b3e071739a957c5852de26b
Pavani Aluri <pavanialuri1775@gmail.com>
2026-06-25 22:19:50 +0530

    Create quick_sort.py
    added quick sort

---
+++ sorting/quick_sort.py
@@ -0,0 +1,27 @@
+#Quick sort:Quick Sort is a divide-and-conquer sorting algorithm that works by:
+#Selecting a pivot element.
+#Partitioning the array so:
+#Elements smaller than the pivot go to the left.
+#Elements larger than the pivot go to the right.
+#Recursively applying the same process to the left and right subarrays.
+#pivot=arr[len(arr)//2]
+
+#code
+def quick_sort(arr):
+    n=len(arr)
+    if n<=1:
+        return arr
+    pivot=arr[n//2]
+    left=[]
+    middle=[]
+    right=[]
+    for x in arr:
+        if x<pivot:
+            left.append(x)
+        elif x==pivot:
+            middle.append(x)
+        else:
+            right.append(x)
+    return quick_sort(left)+middle+quick_sort(right)
+arr=[4,2,6,1,9]
+print(quick_sort(arr))
