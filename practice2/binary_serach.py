a9978cfd8ecdb2ef954a78e591afba804114b0b5
Pavani Aluri <pavanialuri1775@gmail.com>
2026-06-29 05:58:54 +0530

    Added binary search

---
+++ practice2/binary_serach.py
@@ -0,0 +1,18 @@
+#binary search
+#Search for an element in a sorted array using Binary Search.
+def binary_search(arr,target):
+    left=0
+    right=len(arr)-1
+    while left<=right:
+        mid=(left+right)//2
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
+            left=mid+1
+        else:
+            right=mid-1
+    return -1
+arr=list(map(int,input().split()))
+target=int(input())
+result=binary_search(arr,target)
+print(result)
