2553a81deb057e9ae8e5915bd57f4f8ab4728713
Pavani Aluri <pavanialuri1775@gmail.com>
2026-07-08 19:56:55 +0530

    Added

---
+++ sorting/stable_counting.py
@@ -0,0 +1,17 @@
+#class Solution:
+def stable_count_sort(arr):#[2,3,2,4,3]
+    max_ele=max(arr)
+    count=[0]*(max_ele+1)#[0,0,0,0,0]
+    for num in arr:
+        count[num]+=1  #[0,0,2,2,1]
+    for i in range(1,len(count)):
+        count[i]+=count[i-1] #[0,0,2,4,5]
+    output=[0]*len(arr)
+    for i in range(len(arr)-1,-1,-1):
+        ele=arr[i]
+        position=count[ele]-1
+        output[position]=ele
+        count[ele]-=1
+    return output   
+arr=list(map(int,input().split()))
+print(stable_count_sort(arr))
