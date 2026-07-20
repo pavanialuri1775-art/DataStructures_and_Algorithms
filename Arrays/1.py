0d6949cd88df66e112fd2e851ccd4c02d2a3311e
Pavani Aluri <pavanialuri1775@gmail.com>
2026-07-20 20:03:06 +0530

    Added

--- Arrays/1.py
+++ Arrays/1.py
@@ -26,3 +26,21 @@ arr = list(map(int,input().split()))
 
 k=int(input())
 print(long_subarr(arr,k))
+
+#
+def sort012(self, arr):
+    low=0
+    mid=0
+    high=len(arr)-1
+    while mid<=high:
+        if arr[mid]==0:
+            arr[low],arr[mid]=arr[mid],arr[low]
+            low+=1
+            mid+=1
+        elif arr[mid]==1:
+            mid+=1
+        else:
+            arr[mid],arr[high]=arr[high],arr[mid]
+            high-=1
+    return arr
+                
