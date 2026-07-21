e08a5a4a98e3c040b9ec53c7e08e20fa0c3604af
Pavani Aluri <pavanialuri1775@gmail.com>
2026-07-21 22:33:00 +0530

    added

--- Arrays/sliding_window.py
+++ Arrays/sliding_window.py
@@ -1,6 +1,6 @@
 s=['a','i','o', 'r','t','u' ,'o']
 k=4
-vowels="aeiouAEIOU"
+vowels="aeiou"
 win_count=0
 max_count=0
 for i in range(k):
@@ -24,3 +24,16 @@ for i in range(k,len(arr)):
     win_sum+=arr[i]-arr[i-k]
     max_sum=max(win_sum,max_sum)
 print(max_sum)
+
+#
+def is_prime(n):#10
+    if n<=1:
+        return False
+    for i in range(2,int(n**0.5)+1):#2,3
+        if n%i==0:#30%2==0
+            return False
+    return True
+for i in range(2,31):
+    if is_prime(i):
+        print(i,end=" ")#2
+    
