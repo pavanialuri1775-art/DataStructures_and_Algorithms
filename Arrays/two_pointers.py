954b394aaf9c31d5f5d54eff51fbdcb86bf4afe6
Pavani Aluri <pavanialuri1775@gmail.com>
2026-07-23 09:40:03 +0530

    Added two pointers

--- Arrays/two_pointers.py
+++ Arrays/two_pointers.py
@@ -76,6 +76,39 @@ while right<len(b):
     res.append(b[right])
     right+=1
 print(res)
+
+
+#sort 0s and 1s
+arr=list(map(int,input().split()))
+left=0
+for j in range(len(arr)):
+    if arr[j]==0:
+        arr[j],arr[left]=arr[left],arr[j]
+        left+=1
+print(arr)
+
+#
+arr=list(map(int,input().split()))
+left=0
+right=len(arr)-1
+while left<right:
+    if arr[left]==0 and arr[right]==1:
+        left+=1
+        right-=1
+    elif arr[left]==1 and arr[right]==0:
+        arr[left],arr[right]=arr[right],arr[left]
+        left+=1
+        right-=1
+    elif arr[left]==0 and arr[right]==0:
+        left+=1
+    else:
+        right-=1
+print(arr)
+
+
+        
+        
+        
         
         
 
