1493cc74df373b02148002719db4fc024dfc8e2a
Pavani Aluri <pavanialuri1775@gmail.com>
2026-07-26 11:12:48 +0530

    Added queue

---
+++ queue/queue operations.py
@@ -0,0 +1,21 @@
+#Queue
+queue=[]
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+print("Queue:",queue)#Queue: [10, 20, 30]
+
+#Front
+print("Front:", queue[0])#Front: 10
+
+#Dequeue
+print("Removed:", queue.pop(0))#Removed: 10
+
+#Empty Check
+if not queue:
+    print("Queue is empty")#Queue: [20, 30]
+else:
+    print("Queue is not empty")#Queue is not empty
+    
+    
