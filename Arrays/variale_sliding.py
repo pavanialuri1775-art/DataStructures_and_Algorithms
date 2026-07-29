d93340bf4a5b36408ce8ad4d1297a1cafd167316
Pavani Aluri <pavanialuri1775@gmail.com>
2026-07-29 11:52:50 +0530

    Added variable sliding

---
+++ Arrays/variale_sliding.py
@@ -0,0 +1,16 @@
+#variable sliding window:window size varies
+def vari_sliding(arr,k):
+    left=0
+    max_len=0
+    total=0
+    for right in range(len(arr)):
+        total+=arr[right]
+        while total>k:
+            total-=arr[left]
+            left+=1
+        max_len=max(max_len,right-left+1)
+    return max_len
+arr=list(map(int,input().split()))
+k=int(input())
+print(vari_sliding(arr,k))  
+
