9eaca25512f15ff30d4d6023af35edd9c670167f
Pavani Aluri <pavanialuri1775@gmail.com>
2026-09-12 11:31:05 +0530

    Added

---
+++ problem_solving/greedy.py
@@ -0,0 +1,11 @@
+#greedy search:we choose the option  that looks best at the  current moment.
+def coin_cnt(amount,coins):
+    res=[]
+    for coin in coins:
+        while  amount>=coin:
+            amount=amount-coin
+            res.append(coin)
+    return res
+coins=list(map(int,input().split()))
+amt=int(input())
+print(coin_cnt(amt,coins))
