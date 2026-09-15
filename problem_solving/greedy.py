a5fd6dff3ef7948cc3aecc8d97b4752a1ccc0c3e
Pavani Aluri <pavanialuri1775@gmail.com>
2026-09-15 20:13:17 +0530

    Added

--- problem_solving/greedy.py
+++ problem_solving/greedy.py
@@ -9,3 +9,16 @@ def coin_cnt(amount,coins):
 coins=list(map(int,input().split()))
 amt=int(input())
 print(coin_cnt(amt,coins))
+
+
+def fib(n,memo={}):#6,m-5,4
+    if n<=1:
+        return n
+    if n in memo:
+        return memo[n]
+
+    memo[n] = fib(n-1, memo)+fib(n-2,memo)#{6:
+
+    return memo(n)
+
+print(fib(6))
