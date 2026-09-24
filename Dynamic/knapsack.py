f48b29f4c7b359b940e2fb21f9d4fa34baa6dcef
Pavani Aluri <pavanialuri1775@gmail.com>
2026-09-24 19:28:40 +0530

    added

--- Dynamic/knapsack.py
+++ Dynamic/knapsack.py
@@ -5,13 +5,13 @@ def kn(wt,vl,ca):#[2,3,4],[12,20,30],5
 
     n = len(wt)#3
 
-    dp = [[0]*(ca+1) for i in range(n+1)]#mat
+    dp = [[0]*(ca+1) for i in range(n+1)]  #mat
 
     for i in range(1,n+1):#i=1,2,3
 
         for w in range(1,ca+1):#w=1..4,5
 
-            if wt[i-1]<=w:# 4<=5
+            if wt[i-1]<=w:# 4<=5#
 
                 take = vl[i-1]+dp[i-1][w-wt[i-1]] # vl[2]+dp[2][5-4]=30+0=30
 
